bc7b52759ca41ee061275dccf36c8819f00aba5b
Yash Dixit <yashdixit0885@gmail.com>
2022-07-25 15:13:02 -0600

    added a fleet of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,7 @@
 import sys
 
+from random import randint
+
 import pygame
 
 from settings import Settings
@@ -89,22 +91,29 @@ class AlienInvasion:
 
         
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - (2* alien_width)
         number_aliens_x = available_space_x // (2* alien_width)
 
-        #create first row of aliens
+        #Determine the number of rows of aliens that fit on the screen
 
-        for alien_number in range(number_aliens_x):
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - (3*alien_height)- ship_height)
+        number_rows = available_space_y // (2* alien_height)
+
+        #create full fleet of aliens
+
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number,row_number)
+
+    def _create_alien(self, alien_number,row_number):
             alien= Alien(self)
+            alien_width,alien_height = alien.rect.size
             alien.x = alien_width + 2 *alien_width*alien_number
             alien.rect.x = alien.x
-            self.aliens.add(alien)
-
-       
-
-
-
+            alien.rect.y = alien_height + 2*alien.rect.height*row_number
+            self.aliens.add(alien)     
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
