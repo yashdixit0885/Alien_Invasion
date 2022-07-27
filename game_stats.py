6311e59c54199f30c059d8bcc89266e9b0a18c29
Yash Dixit <yashdixit0885@gmail.com>
2022-07-27 16:41:46 -0600

    added high scoring code

--- game_stats.py
+++ game_stats.py
@@ -10,6 +10,9 @@ class Game_Stats:
         #Start Alien Invasion in an inactive state
         self.game_active = False
 
+        # High score should never be reset
+        self.high_score = 0
+
     def reset_stats(self):
         """Initialize statistics that can change during the game"""
         self.ships_left = self.settings.ship_limit
